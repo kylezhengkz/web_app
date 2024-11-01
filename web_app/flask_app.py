215774bde3552a0395775c49611b1a566ef204c8
Kyle <kylezhengkz@gmail.com>
2024-11-01 10:51:46 -0400

    styling fix

--- web_app/flask_app.py
+++ web_app/flask_app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 CORS(app)
 
 def model(data):
-    return {"result": f"{data['input']}"}
+    return {"result": f"{data["input"]}"}
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
     prediction = model(data)
     return jsonify(prediction)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- web_app/static/script.js
+++ web_app/static/script.js
@@ -1,19 +1,18 @@
 async function submitData() {
     try {
-        const inputData = document.getElementById('inputData').value;
-        const resultElement = document.getElementById('result');
-        const response = await fetch('/predict', {
-            method: 'POST',
+        const inputData = document.getElementById("inputData").value;
+        const resultElement = document.getElementById("result");
+        const response = await fetch("/predict", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({ input: inputData })
         });
-        console.log("Reached here");
         const result = await response.json();
         resultElement.innerText = `Prediction: ${result.result}`;
     } catch (error) {
-        resultElement.innerText = 'Error: Could not get prediction.';
-        console.error('Error:', error);
+        resultElement.innerText = "Error: Could not get prediction.";
+        console.error("Error:", error);
     }
 }
